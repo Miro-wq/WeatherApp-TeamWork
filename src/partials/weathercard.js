34745106a0ce20cf57b21c2f25f6215c17d7b57c
Miro <Miro-wq@users.noreply.github.com>
2024-08-05 22:04:15 +0300

    Miro update dupa PR probleme

--- src/partials/weathercard.js
+++ src/partials/weathercard.js
@@ -80,10 +80,8 @@ export function displayFiveDayForecast(data) {
   });
 
   if (forecastContainer) {
-
     forecastContainer.style.display = 'flex';
     forecastContainer.style.justifyContent = 'space-around';
-    forecastContainer.style.display = 'block';
   }
   if (chartContainer) {
     chartContainer.style.display = 'block';
